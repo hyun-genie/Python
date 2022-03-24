d7a4b0cd395a5a2a403cf2ad57a1ef6541a124dc
hyun.genie_ <102285202+hyun-genie@users.noreply.github.com>
2022-03-24 11:24:18 +0000

    gcd,lcm

--- data_structure/GCD,LCM.py
+++ data_structure/GCD,LCM.py
@@ -22,7 +22,6 @@ print(gcd2(a,b))
 # 유클리드 알고리즘 
 # a와 b의 최대공약수는 b와 a를 b로 나눈 나머지의 최대공약수와 같다. 
 # 어떤 수와 0의 최대공약수는 자기 자신이다. 즉, gcd(n,0)=n이다. 
-
 def gcd_u(a,b):
     if b == 0:
       return a 
@@ -30,14 +29,6 @@ def gcd_u(a,b):
 print(gcd_u(a,b))
       
 # 최소공배수(LCM) 
-def lcm(a,b):
-  i = max(a,b)
-  while True:
-    if a%i == 0 and b%i == 0:
-      return i
-    i = (a*b)+1
-print(lcm(a,b))
-
 # 유클리드 이용 : 최소공배수는 a,b의 곱을 a,b의 최대공약수로 나누면 된다. 
 def lcm_u(a,b):
   return (a*b)//gcd_u(a,b)
